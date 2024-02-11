5025dc08696712e9e9cf088662f3eca7724442f7
PregY <iflp1908sl@gmail.com>
2024-02-11 13:35:47 +0100

    alow for using node features

--- torchdrug/tasks/property_prediction.py
+++ torchdrug/tasks/property_prediction.py
@@ -1,5 +1,6 @@
 import math
 from collections import defaultdict
+from collections.abc import Mapping, Sequence
 
 import torch
 from torch import nn
@@ -26,7 +27,7 @@ class PropertyPrediction(tasks.Task, core.Configurable):
         metric (str or list of str, optional): metric(s).
             Available metrics are ``mae``, ``rmse``, ``auprc`` and ``auroc``.
         loss_param (float): parameter in the loss function
-            delta in HUBERLOSS 
+            delta in HUBERLOSS
             or beta in smooth_l1_loss
         num_mlp_layer (int, optional): number of layers in mlp prediction head
         normalization (bool, optional): whether to normalize the target
@@ -35,6 +36,7 @@ class PropertyPrediction(tasks.Task, core.Configurable):
         mlp_dropout (float, optional): dropout in mlp
         graph_construction_model (nn.Module, optional): graph construction model
         verbose (int, optional): output verbose level
+        std_mean (tuple, optional): standard deviation and mean of the target
     """
 
     eps = 1e-10
@@ -54,6 +56,7 @@ class PropertyPrediction(tasks.Task, core.Configurable):
         mlp_dropout=0,
         graph_construction_model=None,
         verbose=0,
+        std_mean=None,
     ):
         super(PropertyPrediction, self).__init__()
         self.model = model
@@ -71,6 +74,22 @@ class PropertyPrediction(tasks.Task, core.Configurable):
         self.mlp_dropout = mlp_dropout
         self.graph_construction_model = graph_construction_model
         self.verbose = verbose
+
+        # modify so that during interference, dummy dataset is not needed
+        # but need specifying task_names, and std_mean manually
+        self.mlp = None
+        if std_mean:
+            self.std = std_mean[0]
+            self.mean = std_mean[1]
+
+        if self.num_class:
+            hidden_dims = [self.model.output_dim] * (self.num_mlp_layer - 1)
+            self.mlp = layers.MLP(
+                self.model.output_dim,
+                hidden_dims + [sum(self.num_class)],
+                batch_norm=self.mlp_batch_norm,
+                dropout=self.mlp_dropout,
+            )
 
     def preprocess(self, train_set, valid_set=None, test_set=None):
         """
@@ -102,19 +121,20 @@ class PropertyPrediction(tasks.Task, core.Configurable):
                     num_class.append(task_class + 1)
             else:
                 num_class.append(1)
-
         self.register_buffer("mean", torch.as_tensor(mean, dtype=torch.float))
         self.register_buffer("std", torch.as_tensor(std, dtype=torch.float))
         self.register_buffer("weight", torch.as_tensor(weight, dtype=torch.float))
         self.num_class = self.num_class or num_class
 
         hidden_dims = [self.model.output_dim] * (self.num_mlp_layer - 1)
-        self.mlp = layers.MLP(
-            self.model.output_dim,
-            hidden_dims + [sum(self.num_class)],
-            batch_norm=self.mlp_batch_norm,
-            dropout=self.mlp_dropout,
-        )
+
+        if self.mlp is None:
+            self.mlp = layers.MLP(
+                self.model.output_dim,
+                hidden_dims + [sum(self.num_class)],
+                batch_norm=self.mlp_batch_norm,
+                dropout=self.mlp_dropout,
+            )
 
     def forward(self, batch):
         """"""
@@ -150,7 +170,12 @@ class PropertyPrediction(tasks.Task, core.Configurable):
                         delta=self.loss_param,
                     )
                 else:
-                    loss = F.huber_loss(pred, target, reduction="none", delta=self.loss_param,)
+                    loss = F.huber_loss(
+                        pred,
+                        target,
+                        reduction="none",
+                        delta=self.loss_param,
+                    )
             elif criterion == "smooth_l1":
                 if self.normalization:
                     loss = F.smooth_l1_loss(
@@ -160,7 +185,12 @@ class PropertyPrediction(tasks.Task, core.Configurable):
                         beta=self.loss_param,
                     )
                 else:
-                    loss = F.smooth_l1_loss(pred, target, reduction="none", beta=self.loss_param,)
+                    loss = F.smooth_l1_loss(
+                        pred,
+                        target,
+                        reduction="none",
+                        beta=self.loss_param,
+                    )
             elif criterion == "bce":
                 loss = F.binary_cross_entropy_with_logits(
                     pred, target, reduction="none"
@@ -682,3 +712,308 @@ class Unsupervised(nn.Module, core.Configurable):
             graph, graph.node_feature.float(), all_loss=all_loss, metric=metric
         )
         return pred
+
+
+# TODO to be able to specify idx for each tasks
+# assume idxs begin from 0
+@R.register("tasks.PropertyPrediction_loc")
+class PropertyPrediction_local(tasks.Task, core.Configurable):
+    """
+    Graph / molecule / protein property prediction task.
+    to have idxs in the graph objects in the first place
+    read any columns with "idxs"
+    This class is also compatible with semi-supervised learning.
+
+    Parameters:
+        model (nn.Module): graph representation model
+        task (str, list or dict, optional): training task(s).
+            For dict, the keys are tasks and the values are the corresponding weights.
+        criterion (str, list or dict, optional): training criterion(s). For dict, the keys are criterions and the values
+            are the corresponding weights. Available criterions are ``mse``, ``huber``, ``smooth_l1``, ``bce`` and ``ce``.
+        metric (str or list of str, optional): metric(s).
+            Available metrics are ``mae``, ``rmse``, ``auprc`` and ``auroc``.
+        loss_param (float): parameter in the loss function
+            delta in HUBERLOSS
+            or beta in smooth_l1_loss
+        num_mlp_layer (int, optional): number of layers in mlp prediction head
+        normalization (bool, optional): whether to normalize the target
+        num_class (int, optional): number of classes
+        mlp_batch_norm (bool, optional): apply batch normalization in mlp or not
+        mlp_dropout (float, optional): dropout in mlp
+        graph_construction_model (nn.Module, optional): graph construction model
+        verbose (int, optional): output verbose level
+    """
+
+    eps = 1e-10
+    _option_members = {"task", "criterion", "metric"}
+
+    def __init__(
+        self,
+        model,
+        task=(),
+        criterion="mse",
+        metric=("mae", "rmse"),
+        loss_param=None,
+        num_mlp_layer=1,
+        normalization=True,
+        num_class=None,
+        mlp_batch_norm=False,
+        mlp_dropout=0,
+        graph_construction_model=None,
+        verbose=0,
+    ):
+        super(PropertyPrediction_local, self).__init__()
+        self.model = model
+        self.task = task
+        self.criterion = criterion
+        self.metric = metric
+        self.loss_param = loss_param
+        self.num_mlp_layer = num_mlp_layer
+        # For classification tasks, we disable normalization tricks.
+        self.normalization = (
+            normalization and ("ce" not in criterion) and ("bce" not in criterion)
+        )
+        self.num_class = (num_class,) if isinstance(num_class, int) else num_class
+        self.mlp_batch_norm = mlp_batch_norm
+        self.mlp_dropout = mlp_dropout
+        self.graph_construction_model = graph_construction_model
+        self.verbose = verbose
+
+    def preprocess(self, train_set, valid_set=None, test_set=None):
+        """
+        Compute the mean and derivation for each task on the training set.
+        """
+        values = defaultdict(list)
+        for sample in train_set:
+            if not sample.get("labeled", True):
+                continue
+            for task in self.task:
+                if not math.isnan(sample[task]):
+                    values[task].append(sample[task])
+        mean = []
+        std = []
+        weight = []
+        num_class = []
+        for task, w in self.task.items():
+            value = torch.tensor(values[task])
+            mean.append(value.float().mean())
+            std.append(value.float().std())
+            weight.append(w)
+            if value.ndim > 1:
+                num_class.append(value.shape[1])
+            elif value.dtype == torch.long:
+                task_class = value.max().item()
+                if task_class == 1 and "bce" in self.criterion:
+                    num_class.append(1)
+                else:
+                    num_class.append(task_class + 1)
+            else:
+                num_class.append(1)
+
+        self.register_buffer("mean", torch.as_tensor(mean, dtype=torch.float))
+        self.register_buffer("std", torch.as_tensor(std, dtype=torch.float))
+        self.register_buffer("weight", torch.as_tensor(weight, dtype=torch.float))
+        self.num_class = self.num_class or num_class
+
+        hidden_dims = [self.model.output_dim] * (self.num_mlp_layer - 1)
+
+        # TODO to adjust MLP size automatically
+        self.mlp = layers.MLP(
+            self.model.output_dim * 2,
+            hidden_dims + [sum(self.num_class)],
+            batch_norm=self.mlp_batch_norm,
+            dropout=self.mlp_dropout,
+        )
+
+    def forward(self, batch):
+        """"""
+        all_loss = torch.tensor(0, dtype=torch.float32, device=self.device)
+        metric = {}
+
+        pred = self.predict(batch, all_loss, metric)
+
+        if all([t not in batch for t in self.task]):
+            # unlabeled data
+            return all_loss, metric
+
+        target = self.target(batch)
+        labeled = ~torch.isnan(target)
+        target[~labeled] = 0
+
+        for criterion, weight in self.criterion.items():
+            if criterion == "mse":
+                if self.normalization:
+                    loss = F.mse_loss(
+                        (pred - self.mean) / self.std,
+                        (target - self.mean) / self.std,
+                        reduction="none",
+                    )
+                else:
+                    loss = F.mse_loss(pred, target, reduction="none")
+            elif criterion == "huber":
+                if self.normalization:
+                    loss = F.huber_loss(
+                        (pred - self.mean) / self.std,
+                        (target - self.mean) / self.std,
+                        reduction="none",
+                        delta=self.loss_param,
+                    )
+                else:
+                    loss = F.huber_loss(
+                        pred,
+                        target,
+                        reduction="none",
+                        delta=self.loss_param,
+                    )
+            elif criterion == "smooth_l1":
+                if self.normalization:
+                    loss = F.smooth_l1_loss(
+                        (pred - self.mean) / self.std,
+                        (target - self.mean) / self.std,
+                        reduction="none",
+                        beta=self.loss_param,
+                    )
+                else:
+                    loss = F.smooth_l1_loss(
+                        pred,
+                        target,
+                        reduction="none",
+                        beta=self.loss_param,
+                    )
+            elif criterion == "bce":
+                loss = F.binary_cross_entropy_with_logits(
+                    pred, target, reduction="none"
+                )
+            elif criterion == "ce":
+                loss = F.cross_entropy(
+                    pred, target.long().squeeze(-1), reduction="none"
+                ).unsqueeze(-1)
+            else:
+                raise ValueError("Unknown criterion `%s`" % criterion)
+            loss = functional.masked_mean(loss, labeled, dim=0)
+
+            name = tasks._get_criterion_name(criterion)
+            if self.verbose > 0:
+                for t, l in zip(self.task, loss):
+                    metric["%s [%s]" % (name, t)] = l
+            loss = (loss * self.weight).sum() / self.weight.sum()
+            metric[name] = loss
+            all_loss += loss * weight
+
+        return all_loss, metric
+
+    # TODO basket here
+    def predict(self, batch, all_loss=None, metric=None):
+        graph = batch["graph"]
+
+        def get_adj_idxs(batch, n_atoms_csum):
+            idx_cols = list(filter(lambda i: "idx" in i, batch))
+            # TODO maybe couldve been better written
+            idxs_adj = []
+            for col in idx_cols:
+                idx_i = batch[col]
+                idx_i[1:] += n_atoms_csum[:-1]
+                idxs_adj.append(idx_i)
+            return idxs_adj
+
+        n_atoms_csum = torch.cumsum(batch["graph"].num_atoms, dim=0)
+        idxs_adj = get_adj_idxs(batch, n_atoms_csum)
+
+        if self.graph_construction_model:
+            graph = self.graph_construction_model(graph)
+        output = self.model(
+            graph, graph.node_feature.float(), all_loss=all_loss, metric=metric
+        )
+        node_feat = output["node_feature"]
+        loc_feats = [node_feat[idx_list] for idx_list in idxs_adj]
+        loc_feats = torch.cat(loc_feats, dim=1)
+        pred = self.mlp(loc_feats)
+        if self.normalization:
+            pred = pred * self.std + self.mean
+        return pred
+
+    def target(self, batch):
+        target = torch.stack([batch[t].float() for t in self.task], dim=-1)
+        labeled = batch.get(
+            "labeled", torch.ones(len(target), dtype=torch.bool, device=target.device)
+        )
+        target[~labeled] = math.nan
+        return target
+
+    def evaluate(self, pred, target):
+        labeled = ~torch.isnan(target)
+
+        metric = {}
+        for _metric in self.metric:
+            if _metric == "mae":
+                score = F.l1_loss(pred, target, reduction="none")
+                score = functional.masked_mean(score, labeled, dim=0)
+            elif _metric == "rmse":
+                score = F.mse_loss(pred, target, reduction="none")
+                score = functional.masked_mean(score, labeled, dim=0).sqrt()
+            elif _metric == "acc":
+                score = []
+                num_class = 0
+                for i, cur_num_class in enumerate(self.num_class):
+                    _pred = pred[:, num_class : num_class + cur_num_class]
+                    _target = target[:, i]
+                    _labeled = labeled[:, i]
+                    _score = metrics.accuracy(_pred[_labeled], _target[_labeled].long())
+                    score.append(_score)
+                    num_class += cur_num_class
+                score = torch.stack(score)
+            elif _metric == "mcc":
+                score = []
+                num_class = 0
+                for i, cur_num_class in enumerate(self.num_class):
+                    _pred = pred[:, num_class : num_class + cur_num_class]
+                    _target = target[:, i]
+                    _labeled = labeled[:, i]
+                    _score = metrics.matthews_corrcoef(
+                        _pred[_labeled], _target[_labeled].long()
+                    )
+                    score.append(_score)
+                    num_class += cur_num_class
+                score = torch.stack(score)
+            elif _metric == "auroc":
+                score = []
+                for _pred, _target, _labeled in zip(
+                    pred.t(), target.long().t(), labeled.t()
+                ):
+                    _score = metrics.area_under_roc(_pred[_labeled], _target[_labeled])
+                    score.append(_score)
+                score = torch.stack(score)
+            elif _metric == "auprc":
+                score = []
+                for _pred, _target, _labeled in zip(
+                    pred.t(), target.long().t(), labeled.t()
+                ):
+                    _score = metrics.area_under_prc(_pred[_labeled], _target[_labeled])
+                    score.append(_score)
+                score = torch.stack(score)
+            elif _metric == "r2":
+                score = []
+                for _pred, _target, _labeled in zip(pred.t(), target.t(), labeled.t()):
+                    _score = metrics.r2(_pred[_labeled], _target[_labeled])
+                    score.append(_score)
+                score = torch.stack(score)
+            elif _metric == "spearmanr":
+                score = []
+                for _pred, _target, _labeled in zip(pred.t(), target.t(), labeled.t()):
+                    _score = metrics.spearmanr(_pred[_labeled], _target[_labeled])
+                    score.append(_score)
+                score = torch.stack(score)
+            elif _metric == "pearsonr":
+                score = []
+                for _pred, _target, _labeled in zip(pred.t(), target.t(), labeled.t()):
+                    _score = metrics.pearsonr(_pred[_labeled], _target[_labeled])
+                    score.append(_score)
+                score = torch.stack(score)
+            else:
+                raise ValueError("Unknown metric `%s`" % _metric)
+
+            name = tasks._get_metric_name(_metric)
+            for t, s in zip(self.task, score):
+                metric["%s [%s]" % (name, t)] = s
+
+        return metric
